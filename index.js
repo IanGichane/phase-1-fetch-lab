9c471568521a86c32e78d48a04db9f77700d96a7
IanGichane <gichaneian@gmail.com>
2023-10-02 12:27:47 +0300

    Render json

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  //make a request to an API
+  //APIs allow us to add data and functionalityto the applications we build
   
+  fetch('https://anapioficeandfire.com/api/books')//fetch data
+  .then((resp)=>resp.json())//render the APIs response in plain js
+  .then((books)=>renderBooks(book))//print out the data retrieved
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -15,3 +19,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+//JSON a technique for formationg data
